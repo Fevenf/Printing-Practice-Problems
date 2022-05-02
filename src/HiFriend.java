033f7d95835a32425fe54b82feea2d98459fc5fd
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-05-02 14:09:20 -0400

    first commit

---
+++ src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String Feven= "Hi" + " Feven" +" Fenta!";  
+        System.out.println(Feven);
+      
+}  
+}
